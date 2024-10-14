83aa369aaf5edf1bc0dc9dc7178bedcc75fb857a
Vigneshprabhu M <141343624+vignesh-in04@users.noreply.github.com>
2024-10-14 21:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 visitor_count = 0
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/portfolio')
 def portfolio():
